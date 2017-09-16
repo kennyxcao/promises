60c5c51e3e05fc28352ed323ff14ff8c67ef82d3
Hack Reactor Student <student@hackreactor.com>
2017-09-16 10:38:15 -0700

    complete with callback

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -1,18 +1,23 @@
 /**
  * Implement these functions following the node style callback pattern
  */
-
 var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, function(err, data) {
+
+    callback(err, data ? data.toString().split('\n')[0] : undefined);
+
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function(err, res, body) {
+    callback(err, err ? undefined : res.statusCode);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
